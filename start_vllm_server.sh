32d5ff43757d131b9dbaa66cb70d53547dcf128e
Nam Bui <namb@koa-dgxa-b10-u11.localdomain>
2024-12-07 16:01:52 +0000

    oke

---
+++ start_vllm_server.sh
@@ -0,0 +1,11 @@
+cd /home/namb/nambkh 
+CUDA_VISIBLE_DEVICES=5,6,7,8 \
+vllm serve Llama-3.3-70B-Instruct \
+--dtype bfloat16 \
+--disable-log-stats \
+--enable-chunked-prefill \
+--gpu-memory-utilization 0.9 \
+--host 0.0.0.0 \
+--port 8148 \
+--seed 148 \
+--tensor-parallel-size 4 \
